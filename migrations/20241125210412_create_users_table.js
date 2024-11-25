5b30e3a955677f8f7f87781f99b8dc3403f85687
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-11-25 15:48:25 -0800

    create structure for users table

--- migrations/20241125210412_create_users_table.js
+++ migrations/20241125210412_create_users_table.js
@@ -3,7 +3,16 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-  
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('location').notNullable();
+        table.string('email').unique.notNullable();
+        table.string('password').notNullable();
+        table.string('password_confirmation').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
+      });
 };
 
 /**
@@ -11,5 +20,5 @@ export function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-  
+    return knex.schema.dropTableIfExists('users');
 };
