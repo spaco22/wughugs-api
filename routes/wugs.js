c8c90e3416e2b0bf4802f06bead0cd9abf8f04ab
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-12-02 15:12:07 -0800

    add multer to wugs route

--- routes/wugs.js
+++ routes/wugs.js
@@ -1,11 +1,17 @@
 import express from "express";
 import * as wugsController from "../controllers/wugs-controller.js";
 import multer from "multer";
-import path from "path";
 
-const upload = multer({
-    dest: path.resolve('public/images'),
+const storage = multer.diskStorage({
+  destination: "public/images",
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now();
+    const uploadedfileName = uniqueSuffix + "-" + file.originalname;
+    cb(null, uploadedfileName);
+  },
 });
+
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
