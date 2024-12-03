792b5ef05a0bc158ca282b692a99347e5e041bfd
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-12-03 15:18:38 -0800

    remove username and wugname as they are not needed and can later be joined

--- migrations/20241203230931_create_journals_table.js
+++ migrations/20241203230931_create_journals_table.js
@@ -7,8 +7,6 @@ export function up(knex) {
         table.increments('journal_id').primary();
         table.integer('wug_id').unsigned().notNullable().references('wug_id').inTable("wugs").onDelete("CASCADE");
         table.integer('user_id').unsigned().notNullable().references('user_id').inTable("users").onDelete("CASCADE");
-        table.integer('user_name').unsigned().notNullable().references('user_name').inTable("users").onDelete("CASCADE");
-        table.integer('wug_name').unsigned().notNullable().references('wug_name').inTable("wugs").onDelete("CASCADE");
         table.string('title').notNullable(); 
         table.text('text').notNullable();
         table.timestamp('date_created').notNullable().defaultTo(knex.fn.now()); 
