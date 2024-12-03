88aa980d5636e1da35378bb69b99fd2ff44401b1
spaco22 <58753896+spaco22@users.noreply.github.com>
2024-12-03 15:32:34 -0800

    create get journals data

--- controllers/journals-controller.js
+++ controllers/journals-controller.js
@@ -3,17 +3,17 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 const journals = async (req, res) => {
-    res.send("This is the journals route");
-//     try {
-//       const usersData = await knex("users");
-//       res.status(200).json(usersData);
-//     } catch (error) {
-//       console.error("Error retrieving users data", error);
-//       res.status(400).json({
-//         message: "Error retrieving users data",
-//         status: 400,
-//       });
-//     }
+    // res.send("This is the journals route");
+    try {
+      const journalsData = await knex("journals");
+      res.status(200).json(journalsData);
+    } catch (error) {
+      console.error("Error retrieving journals data", error);
+      res.status(400).json({
+        message: "Error retrieving journals data",
+        status: 400,
+      });
+    }
   };
 
 export { journals };
